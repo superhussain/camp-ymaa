6d27b2231ab892e545c2855742d2da9749bde697
Hussain Abbas <hussain@hussainabbas.com>
2016-03-10 03:33:23 -0500

    testimonials

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
     contactForm();
     slider();
     navScroll();
+    testimonialScroller();
     $(window).scroll(function() {
       // functions on scroll
       navScroll();
@@ -114,3 +115,53 @@ var navScroll = function() {
     $('nav').removeClass('scrolled');
   }
 }
+
+var testimonialScroller = function() {
+  var $quote = $('#testimonials .quote');
+  var $text = $('#testimonials .quote .quote-text');
+  var $author = $('#testimonials .quote .quote-author span');
+  setInterval(function() {
+    var indexNum = Math.floor(Math.random() * testimonials.length);
+    var rand = testimonials[indexNum];
+    $quote.fadeOut(function() {
+      $(this).fadeIn();
+      $text.text(rand.text).fadeIn();
+      $author.text(rand.author).fadeIn();
+    });
+  }, 10000);
+}
+
+var testimonials = [
+  {
+    "text": "I personally feel that YMAA camp is an exceptional value as it provides halal channels of socializing, energizing and enhancing the knowledge of children. I would not hesitate to send my children to YMAA camp again next year.",
+    "author": "Abbas"
+  },
+  {
+    "text": "The best thing my child gained at the camp was that he got to interact with other children and adults and learn to be in an Islamic environment The camp offers a unique blend of challenging, enjoyable activities through which children grow. The staff take their jobs seriously and strive to do their best. We feel comfortable knowing our kids are somewhere safe, and fun.",
+    "author": "Anonymous"
+  },
+  {
+    "text": "A very well thought out and organized camp covering all areas of YMAA's motto in promoting a healthy mind, body and soul. A lovely and dedicated team of counselors, keep up the great work. Looking forward to more camps!",
+    "author": "Anonymous"
+  },
+  {
+    "text": "The camp was very conveniently located right off the 404. Camp counselors are friendly and polite. Kids had a blast each day!",
+    "author": "Shams Ahsan"
+  },
+  {
+    "text": "Hani loved his experience at the camp. He enjoyed all the activities. The activities were varied enough to keep his interest, and involve all aspects of learning. What's appreciated even more is that it strengthens the child's attachment to Muslim community, make Muslim friends and increases his religious knowledge, and applies the daily Islamic way of life, which is something that is deeply needed more than anything else these days. In addition to the usual activities of a camp, the involvement and participation in Muslim community, making Muslim friends at his age, and going through an Islamic daily routine including prayers and duas is extremely valuable and influential, especially at his age.",
+    "author": "Al Radif Family"
+  },
+  {
+    "text": "My son's experience at Camp was amazing. He enjoyed going there every day for two weeks, made new friends, learned many new academic, social, physical, and Islamic skills/knowledge. The coordinator and counselors were caring, friendly, and so much fun!! I will be sending my son next year and would highly recommend the best camp ever!",
+    "author": "Jaffer Family"
+  },
+  {
+    "text": "They made a lot of friends and gained a Lot of knowledge!",
+    "author": "Rajabali Family"
+  },
+  {
+    "text": "The fact that she never stopped talking about what she did at camp said it all for me. Loved how much she looked forward to going in the mornings. She loved the new friends she made and enjoyed games such as broken telephone and dodge-ball. I was thrilled to see her learn how to dribble a basketball and understand the game of soccer. Way to go guys you all ROCK!!!!!! Thanks for all the care, support and knowledge you imparted. May Allah bless all of you for always having the children's best interest at heart.",
+    "author": "Najarali Family"
+  }
+];
